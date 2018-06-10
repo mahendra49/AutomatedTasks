aba2d5742b85f4509f77f4c0a0b99d1cff1d75e5
mahendra <mahendrasuthar02@gmail.com>
2018-06-10 16:31:50 +0530

    exceptions handled

--- compareprice.py
+++ compareprice.py
@@ -1,7 +1,7 @@
-# this script will compare prices of a product on amazon and flipkart and display the best one
+#this script will compare price of a product on amazon and flipkart and display the better one
 #be specific with the product name 
 import sys,requests,bs4,webbrowser,re
-print("----------comparing prices...-----------")
+print("----------comparing ...-----------")
 #search google for the product
 areq=requests.get('http://google.com/search?q='+'amazon '+''.join(sys.argv[1:]))
 freq=requests.get('http://google.com/search?q='+'flipkart '+''.join(sys.argv[1:]))
@@ -15,27 +15,44 @@ alist=asoup.select('.r a') #select all the links
 areq=requests.get('http://google.com'+alist[0].get('href')) 
 asoup=bs4.BeautifulSoup(areq.text)
 aAmountlist=asoup.select('#priceblock_dealprice')
+a=False
+f=False
 #price on amazon is:
-print('amazon price------> '+aAmountlist[0].getText())
-atmp=aAmountlist[0].getText().split('.')
-acost=re.sub('[^0-9]','',atmp[0])
-acost=int(acost)
-
+try:
+  print('amazon price------> '+aAmountlist[0].getText())
+  atmp=aAmountlist[0].getText().split('.')
+  acost=re.sub('[^0-9]','',atmp[0])
+  acost=int(acost)
+  a=True
+except: #product not found
+  print("product not found on amazon")
 #scraping the flipkart page
 #print('wait wait...')
 flist=fsoup.select('.r a')
 freq=requests.get('http://google.com'+flist[0].get('href'))
 fsoup=bs4.BeautifulSoup(freq.text)
 fAmountlist=fsoup.select('._1vC4OE._3qQ9m1')
-print('flipkart price----->'+fAmountlist[0].getText())
-fcost=re.sub('[^0-9]','',fAmountlist[0].getText()) #price on flipkart
-fcost=int(fcost)
+try:
+  print('flipkart price----->'+fAmountlist[0].getText())
+  fcost=re.sub('[^0-9]','',fAmountlist[0].getText()) #price on flipkart
+  fcost=int(fcost)
+  f=True
+except:
+  print("product not found on flipkart")
 
-if acost<=fcost:  #:P
-    print("fetching amazon page...")
-    webbrowser.open('http://google.com'+alist[0].get('href'))
-else:
-    print("fetching flipkart page..")
-    webbrowser.open('http://google.com'+flist[0].get('href'))
+if a and f:
+  if acost<=fcost:  #:P
+      print("fetching amazon page...")
+      webbrowser.open('http://google.com'+alist[0].get('href'))
+  else:
+      print("fetching flipkart page..")
+      webbrowser.open('http://google.com'+flist[0].get('href'))
 
+elif a:
+  print("fetching amazon page...")
+  webbrowser.open('http://google.com'+alist[0].get('href'))    
+
+elif f:
+  print("fetching flipkart page..")
+  webbrowser.open('http://google.com'+flist[0].get('href'))  
 
